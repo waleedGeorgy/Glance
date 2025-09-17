9b0bf07d4b52b7f63e4136aa0dc8df70c58c6385
Waleed Georgy <49490582+waleedGeorgy@users.noreply.github.com>
2025-09-17 20:24:45 +0300

    Production ready

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 import authRouter from "./routes/auth.route.ts";
 import userRouter from "./routes/user.route.ts";
 import postsRouter from "./routes/post.route.ts";
@@ -14,6 +15,8 @@ const SERVER_PORT = process.env.SERVER_PORT;
 
 const app = express();
 
+const __dirname = path.resolve();
+
 app.use(express.json({ limit: "3mb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
@@ -23,6 +26,13 @@ app.use(
     credentials: true,
   })
 );
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  
+  app.get(/^(?!\/api).*/, (req, res) => {
+    res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+  });
+}
 
 app.use("/api/auth", authRouter);
 app.use("/api/users", userRouter);
